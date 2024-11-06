dacf9716fe39459f16b2813b9f45027cd2cb75bb
igvc team <blaine.oania@gmail.com>
2024-11-05 22:31:38 -0500

    seperated robot bringup from visualization

---
+++ shanti_description/launch/bringup.launch.py
@@ -0,0 +1,20 @@
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='shanti_description').find('shanti_description')
+    default_model_path = os.path.join(pkg_share, 'src/description/shanti_description.urdf')
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    )
+
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        robot_state_publisher_node,
+    ])
--- shanti_description/launch/display.launch.py
+++ shanti_description/launch/display.launch.py
@@ -8,16 +8,11 @@ def generate_launch_description():
     default_model_path = os.path.join(pkg_share, 'src/description/shanti_description.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
 
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
-    )
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', default_model_path])}],
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}],
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
     joint_state_publisher_gui_node = launch_ros.actions.Node(
@@ -43,6 +38,5 @@ def generate_launch_description():
                                             description='Absolute path to rviz config file'),
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
-        robot_state_publisher_node,
         rviz_node
     ])
